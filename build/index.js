e640cb1b801a22e17c1c77ff2825e2737e8652f8
beataj <beata@emc2wave.com>
2021-05-10 19:03:22 -0700

    create sort()

--- build/index.js
+++ build/index.js
@@ -3,6 +3,20 @@ var Solter = /** @class */ (function () {
     function Solter(collection) {
         this.collection = collection;
     }
+    Solter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
     return Solter;
 }());
 var solter = new Solter([10, 3, -5, 0]);
+solter.sort();
+console.log(solter.collection);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,22 @@ class Solter {
 
     constructor(public collection: number[]) {
     }
+
+    sort(): void {
+        const { length } = this.collection;
+
+        for(let i = 0; i < length; i++) {
+            for(let j = 0; j < length - i - 1; j++) {
+                if(this.collection[j] > this.collection[j +1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j +1] = leftHand;
+                }
+            }
+        }
+    }
 }
 
 const solter = new Solter([10,3,-5,0]);
+solter.sort();
+console.log(solter.collection);
