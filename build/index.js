8a62d058d0066eef3743491632b19c6fc4d63b90
beataj <beata@emc2wave.com>
2021-05-12 15:45:37 -0700

    export class Sorter

--- build/index.js
+++ build/index.js
@@ -1,26 +1,4 @@
 "use strict";
-var Solter = /** @class */ (function () {
-    function Solter(collection) {
-        this.collection = collection;
-    }
-    Solter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                if (typeof this.collection === 'string') {
-                }
-            }
-        }
-    };
-    return Solter;
-}());
 var solter = new Solter([10, 3, -5, 0]);
 solter.sort();
 console.log(solter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,4 @@
-class Solter {
 
-    constructor(public collection: number[] | string) {
-    }
-
-    sort(): void {
-        const { length } = this.collection;
-
-        for(let i = 0; i < length; i++) {
-            for(let j = 0; j < length - i - 1; j++) {
-
-                if(this.collection instanceof Array) {
-                    if(this.collection[j] > this.collection[j +1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j +1] = leftHand;
-                    }
-               }
-
-               if(typeof this.collection === 'string') {
-                   
-               }
-
-            }
-        }
-
-    }
-}
 
 const solter = new Solter([10,3,-5,0]);
 solter.sort();
