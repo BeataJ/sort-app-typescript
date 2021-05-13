06d2c28e9fd94e9fa0c10058118275f653fceb32
beataj <beata@emc2wave.com>
2021-05-12 23:25:22 -0700

    create class Node class LinkedList

--- build/LinkedList.js
+++ build/LinkedList.js
@@ -1,1 +1,16 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LinkedList = void 0;
+var Node = /** @class */ (function () {
+    function Node(data) {
+        this.data = data;
+        this.next = null;
+    }
+    return Node;
+}());
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+    }
+    return LinkedList;
+}());
+exports.LinkedList = LinkedList;
