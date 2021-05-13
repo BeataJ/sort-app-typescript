a9cf93893389002f4d542775ec738c53d321a870
beataj <beata@emc2wave.com>
2021-05-12 23:13:30 -0700

    init CharactersCollection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection'
 
 const numbersCollection = new NumbersCollection([10,3,-5,0])
+const charactersCollection = new CharactersCollection('beata')
 const solter = new Sorter(numbersCollection);
+const solter1 = new Sorter(charactersCollection)
 solter.sort();
+solter1.sort()
 console.log(numbersCollection.data);
+console.log(charactersCollection.data);
